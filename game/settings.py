891a06a13fc573cfd263f6f6d5a776c1a44a8a05
Paul <97823475+paulaleksander@users.noreply.github.com>
2022-03-05 14:56:40 +0300

    create Settings class

---
+++ game/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """A class to store all setiings for Alien Invasion."""
+
+    def __init__(self) -> None:
+        """Initialise the game's settings"""
+        # Screen settings
+        self.screen_width = 1280
+        self.screen_height = 720
+        self.bg_color = (0, 0, 139)
