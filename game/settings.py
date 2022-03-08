f36a266a92d349829051dd2106e1a35ae8ba57a5
Paul <97823475+paulaleksander@users.noreply.github.com>
2022-03-08 15:23:50 +0300

    Update alien_invasion
    Adding unit moves
    adding bullets

--- game/settings.py
+++ game/settings.py
@@ -7,3 +7,16 @@ class Settings:
         self.screen_width = 1280
         self.screen_height = 720
         self.bg_color = (0, 0, 139)
+
+        # Ship settings
+        self.ship_speed = 1.5
+
+        # UFO settings
+        self.ufo_speed = 2
+
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (255, 0, 204)
+        self.bullets_allowed = 3
